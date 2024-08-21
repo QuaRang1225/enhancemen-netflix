6d40cf465aa7248b6895244edaaba9f974308b87
QuaRang <dbduddnd1225@gmail.com>
2024-08-21 16:35:21 +0900

    초기화

---
+++ EX_Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EX_Netflix
+//
+//  Created by 유영웅 on 8/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EX_NetflixUITests/EX_NetflixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EX_NetflixUITestsLaunchTests.swift
+//  EX_NetflixUITests
+//
+//  Created by 유영웅 on 8/21/24.
+//
+
+import XCTest
+
+final class EX_NetflixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
