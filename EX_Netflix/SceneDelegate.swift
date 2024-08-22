e447a7792398766d5812d96eeba5941c17bdb187
QuaRang <dbduddnd1225@gmail.com>
2024-08-22 10:40:32 +0900

    한 씬의 윈도우가 두개일때 테스트
    - 한 씬의 윈도우가 두개일때 어떤 메서드부터 생성되고 사라지는지를 확인하는 브랜치 생성

--- EX_Netflix/SceneDelegate.swift
+++ EX_Netflix/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-//    var window1: UIWindow?
+    var window1: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -23,10 +23,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.rootViewController = ViewController()
         self.window?.makeKeyAndVisible()    //화면 세팅
         
-//        self.window1 = UIWindow(windowScene: window)
-//        self.window1?.windowScene = window
-//        self.window1?.rootViewController = ViewController1()
-//        self.window1?.makeKeyAndVisible() //화면 세팅
+        self.window1 = UIWindow(windowScene: window)
+        self.window1?.windowScene = window
+        self.window1?.rootViewController = ViewController1()
+        self.window1?.makeKeyAndVisible() //화면 세팅
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
