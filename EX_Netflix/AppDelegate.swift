519455241a17f1cd973ed24dffbacff65916cfb2
QuaRang <dbduddnd1225@gmail.com>
2024-08-22 10:35:32 +0900

    실험
    - 앱 생명주기 메서드 테스트
    - 씬 생명주기 메서드 테스트
    - 뷰 생명주기 메서드 테스트

--- EX_Netflix/AppDelegate.swift
+++ EX_Netflix/AppDelegate.swift
@@ -11,9 +11,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("앱 초기화 및 구동 프로세스 시작")
+        return true
+    }
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("앱 구동 프로세스 완료")
         return true
     }
 
@@ -22,10 +26,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        print("씬 생성 시 필요한 설정 데이터 가져옴")
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        print("씬 세션 종료")
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
--- EX_Netflix/ViewController.swift
+++ EX_Netflix/ViewController.swift
@@ -11,9 +11,65 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let view1 = UIView(frame: CGRect(x: 30, y: 30, width: 100, height: 100))
+        view1.backgroundColor = .red
+        self.view.addSubview(view1)
+        print("뷰 업로드됨")
         // Do any additional setup after loading the view.
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("뷰 나타날 예정")
+    }
+    override func viewIsAppearing(_ animated: Bool) {
+        print("뷰 나타나는 중")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("뷰 닫힘")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("뷰 닫힐 예정")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("뷰 나타남")
+    }
+    override func viewWillLayoutSubviews() {
+        print("레이아웃 구성 예정")
+    }
+    override func viewDidLayoutSubviews() {
+        print("레이아웃 구성 완료")
+    }
 }
 
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let view1 = UIView(frame: CGRect(x: 130, y: 130, width: 100, height: 100))
+        view1.backgroundColor = .blue
+        self.view.addSubview(view1)
+        print("뷰 업로드됨1")
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("뷰 나타날 예정1")
+    }
+    override func viewIsAppearing(_ animated: Bool) {
+        print("뷰 나타나는 중1")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("뷰 닫힘1")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("뷰 닫힐 예정1")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("뷰 나타남1")
+    }
+    override func viewWillLayoutSubviews() {
+        //앱이 백그라운드 진입 시 레이아웃 정보가 바뀌는 듯함
+        print("레이아웃 구성 예정1")
+    }
+    override func viewDidLayoutSubviews() {
+        print("레이아웃 구성 완료1")
+    }
+}
